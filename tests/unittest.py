b4f20471f8ef6008b89cc7b537707dbe31deae9a
Elisa Petit-Sirigu <E.Petit.inf21@stud.bbbaden.ch>
2023-08-25 16:34:04 +0200

    finished first unittest

---
+++ tests/unittest.py
@@ -0,0 +1,17 @@
+import unittest
+import helper
+import main
+
+class TestMethod1(unittest.TestCase):
+    def test_replace1(self):
+        self.assertEqual('b'.replace(), 'BBB')
+
+    def test_check(self):
+        self.assertTrue('BBB'.isupper())
+        self.assertFalse('b'.isupper())
+
+    def test_final(self):
+        b = 'baden'
+        self.assertEqual(b.bbb(), ['b', 'BBB'])
+        with self.assertRaises(TypeError):
+            b.bbb("BBB")
